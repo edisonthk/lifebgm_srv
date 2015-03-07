e4e1a9cdce0988cb565b0156082bde0bf2ebb24a
edisonthk-mac <edisonthk@gmail.com>
2015-03-08 06:43:31 +0900

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_states_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('states', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('account_id');
+			$table->string('condition');
+			$table->string('locale');
+			$table->string('action');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('states');
+	}
+
+}
---
+++ database/seeds/StateTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use App\State;
+
+class StateTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		State::create(['action'=>'wakeup','condition'=>'{"time_from":"07:00","time_from":"09:00","sensors":["sunlight"]}','locale'=>'jp','account_id' => '1']);
+		State::create(['action'=>'breakfast','condition'=>'{"time_from":"07:00","time_from":"09:00","sensors":["kettle"]}','locale'=>'jp','account_id' => '1']);
+		State::create(['action'=>'goingout','condition'=>'{"time_from":"07:00","time_from":"09:00","sensors":["frontdoor"],"location":{"lat":35.914646,"lng":139.905797,"rad":0.0002}}','locale'=>'jp','account_id' => '1']);
+		State::create(['action'=>'comingback','condition'=>'{"time_from":"18:00","time_from":"24:00","sensors":["frontdoor"],"location":{"lat":35.914646,"lng":139.905797,"rad":0.0002}}','locale'=>'jp','account_id' => '1']);
+		State::create(['action'=>'programming','condition'=>'{"time_from":"18:00","time_from":"24:00","sensors":["desk"]}','locale'=>'jp','account_id' => '1']);
+		State::create(['action'=>'sleep','condition'=>'{"time_from":"24:00","time_from":"05:00","sensors":["dark"]}','locale'=>'jp','account_id' => '1']);
+	}
+
+}
